7147de3f267bab2262cdae133f7982f65b15cc24
romo1102 <romo1102@student.miun.se>
2016-01-26 14:03:08 +0100

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "utilities.hpp"
+
+int main(int argc, char** argv)
+{
+	std::cout << "Anropar print()" << std::endl;
+	std::string s("john carlos!!");
+	print(s);
+	std::cout << "dayum..." << std::endl;
+	return 0;
+}
---
+++ utilities.hpp
@@ -0,0 +1,12 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+
+#include <iostream>
+#include <string>
+
+void print(std::string & s)
+{
+	std::cout << s;
+}
+
+#endif
