496a30679e49cbef590dc20d2358241363beedd5
Jonathan Olsson <jool1403@student.miun.se>
2016-01-26 14:31:39 +0100

    Ett utropstecken
    Jag tog bort ett utropstecken för att det inte ska skrika så mycket.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char** argv)
 {
 	std::cout << "Anropar print()" << std::endl;
-	std::string s("john carlos!!");
+	std::string s("john carlos!");
 	print(s);
 	std::cout << "dayum..." << std::endl;
 	return x;
